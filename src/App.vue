11f3354d7cf6b982e7d6c97f3256fdd7efc57d8d
nicolafelice <felicenicola99@hotmail.com>
2021-09-29 15:57:32 +0200

    add: header component + imgs

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div id="app">
-
+    <Header/>
   </div>
 </template>
 
 
 <script>
-
+import Header from "@/components/Header.vue";
 export default {
   name: 'App',
   components: {
-    //
+    Header
   }
 }
 </script>
 
 
 <style lang="scss">
+@import "@/assets/common.scss";
+
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <header>
+    <div class="container">
+      <div class="logo_wrapper">
+        <img class="logo" src="../assets/img/dc-logo.png" alt="DC logo">
+      </div>
+
+      <nav class="header_nav">
+        <ul>
+          <li><a href="#">comics</a></li>
+          <li><a class="active" href="#">comics</a></li>
+          <li><a href="#">comics</a></li>
+          <li><a href="#">comics</a></li>
+          <li><a href="#">comics</a></li>
+          <li><a href="#">comics</a></li>
+        </ul>
+      </nav>      
+    </div>
+  </header>
+
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+
+<style scoped lang="scss">
+@import "@/assets/variables.scss";
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.logo {
+  width: 70px;
+}
+
+.header_nav ul {
+  display: flex;
+  list-style: none;
+
+  a {
+    color: black;
+    text-decoration: none;
+    display: block;
+    padding: 2.2rem 0;
+    margin: 0 1rem;
+    text-transform: uppercase;
+
+    &.active {
+      border-bottom: 5px $main_color solid;
+    }
+  }
+}  
+</style>
